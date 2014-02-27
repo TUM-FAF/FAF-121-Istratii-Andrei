485bc890b1acb54a2151bbc9bc24c8f3aad28a7d
Andrew <andrei.istratii@gmail.com>
2014-02-27 22:54:04 +0200

    register winclasses and setup basic winprocs

--- WP/lab#3/source/WP_Lab3/winmain.cpp
+++ WP/lab#3/source/WP_Lab3/winmain.cpp
@@ -9,17 +9,25 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #include <commctrl.h>
 #include <map>
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <regex>
 
+#include <io.h>
+#include <cstdio>
+#include <fcntl.h>
 
 using namespace std;
 
 
 map<string, string> g_options;
-
+LPCWSTR g_mainWinClassName = L"lab3";
+LPCWSTR g_workspaceWinClassName = L"workspace";
 
 
 LRESULT CALLBACK MainWinProc(HWND, UINT, WPARAM, LPARAM);
-ATOM RegisterMainWindow(HINSTANCE, LPCWSTR);
+LRESULT CALLBACK WorkspaceProc(HWND, UINT, WPARAM, LPARAM);
+ATOM RegisterMainWindow(HINSTANCE, LPCWSTR, WNDPROC);
 bool LoadConfigFile(char const * filePath);
 
 void CreateDeveloperConsole();
@@ -27,7 +35,8 @@ void CreateDeveloperConsole();
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
 {
-    LPCWSTR mainWinClassName = L"lab2";
+
+
     HWND hWnd;
     MSG msg;
 
@@ -43,12 +52,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
     if (!LoadConfigFile("settings.cfg"))
     {
         MessageBox(NULL, L"Could not load config file", L"Error", MB_OK);
+        return 0;
     }
 
 
-    if (!RegisterMainWindow(hInstance, mainWinClassName))
+    if (!RegisterMainWindow(hInstance, g_mainWinClassName, MainWinProc))
     {
-        MessageBox(NULL, L"Could not register window class!", L"Error!", MB_OK|MB_ICONERROR);
+        MessageBox(NULL, L"Could not register main window class!", L"Error!", MB_OK|MB_ICONERROR);
+        return 0;
+    }
+
+    if (!RegisterMainWindow(hInstance, g_workspaceWinClassName, WorkspaceProc))
+    {
+        MessageBox(NULL, L"Could not register workspace window class!", L"Error!", MB_OK|MB_ICONERROR);
         return 0;
     }
 
@@ -62,9 +78,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
     AdjustWindowRectEx(&rct, WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL, TRUE, WS_EX_OVERLAPPEDWINDOW);
 
     hWnd = CreateWindowEx(WS_EX_OVERLAPPEDWINDOW,
-                            mainWinClassName,
-                            L"WP Lab 2",
-                            WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_HSCROLL | WS_VSCROLL, //WS_OVERLAPPEDWINDOW,
+                            g_mainWinClassName,
+                            L"WP Lab 3",
+                            WS_OVERLAPPEDWINDOW,
                             CW_USEDEFAULT, 0,
                             rct.right - rct.left, rct.bottom - rct.top,
                             NULL, NULL,
@@ -90,5 +106,161 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
     }
 
     return msg.wParam;
+}
+
+
+
+LRESULT CALLBACK MainWinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static HWND hWorkspace = NULL;
+
+    switch (message)
+    {
+    case WM_CREATE:
+        {
+            HINSTANCE hInstance = (HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE);
+
+            RECT rct;
+            GetClientRect(hWnd, &rct);
+
+            hWorkspace = CreateWindowEx(WS_EX_CLIENTEDGE,
+                                        g_workspaceWinClassName,
+                                        L"",
+                                        WS_CHILD|WS_VISIBLE|WS_HSCROLL|WS_VSCROLL,
+                                        120, 20,
+                                        rct.right - rct.left - 140, rct.bottom - rct.top - 40,
+                                        hWnd, NULL,
+                                        hInstance, NULL);
+        }
+        break;
+
+    case WM_SIZE:
+        {
+            int clientWidth = LOWORD(lParam);
+            int clientHeight = HIWORD(lParam);
+
+            SetWindowPos(hWorkspace, HWND_TOP, 0, 0, clientWidth - 140, clientHeight - 40, SWP_NOMOVE|SWP_NOREPOSITION);
+        }
+        break;
+
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+
+
+LRESULT CALLBACK WorkspaceProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+
+    switch (message)
+    {
+    case WM_CREATE:
+
+        break;
+
+
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+
+
+ATOM RegisterMainWindow(HINSTANCE hInstance, LPCWSTR className, WNDPROC proc)
+{
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.lpfnWndProc = proc;
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+    wcex.hCursor        = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_ARROW));
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW);
+    wcex.lpszMenuName   = className;
+    wcex.lpszClassName  = className;
+    wcex.hIconSm        = 0;
+
+    return RegisterClassEx(&wcex);
+}
+
+
+void CreateDeveloperConsole()
+{
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    int consoleHandleR, consoleHandleW ;
+    long stdioHandle;
+    FILE *fptr;
+
+    AllocConsole();
+    std::wstring strW = L"Dev Console";
+    SetConsoleTitle( strW.c_str() );
+
+    EnableMenuItem(GetSystemMenu(GetConsoleWindow(), FALSE), SC_CLOSE , MF_GRAYED);
+    DrawMenuBar(GetConsoleWindow());
+
+    GetConsoleScreenBufferInfo( GetStdHandle(STD_OUTPUT_HANDLE), &consoleInfo );
+
+    stdioHandle = (long)GetStdHandle( STD_INPUT_HANDLE );
+    consoleHandleR = _open_osfhandle( stdioHandle, _O_TEXT );
+    fptr = _fdopen( consoleHandleR, "r" );
+    *stdin = *fptr;
+    setvbuf( stdin, NULL, _IONBF, 0 );
+
+    stdioHandle = (long) GetStdHandle( STD_OUTPUT_HANDLE );
+    consoleHandleW = _open_osfhandle( stdioHandle, _O_TEXT );
+    fptr = _fdopen( consoleHandleW, "w" );
+    *stdout = *fptr;
+    setvbuf( stdout, NULL, _IONBF, 0 );
+
+    stdioHandle = (long)GetStdHandle( STD_ERROR_HANDLE );
+    *stderr = *fptr;
+    setvbuf( stderr, NULL, _IONBF, 0 );
+}
+
+
+
+bool LoadConfigFile(char const * filePath)
+{
+    ifstream configFile(filePath, ifstream::in);
+
+    if (configFile.fail())
+    {
+        return false;
+    }
+
+    regex rgx("(\\w+)\\s*=\\s*(\\w+)");
+    smatch results;
+
+    while (configFile.good())
+    {
+        string str;
+        getline(configFile, str);       
+
+        if (regex_search(str, results, rgx))
+        {
+            // cout << results[1] << " <-> " << results[2] << endl;
+            g_options[results.str(1)] = results.str(2);
+        }
+    }
+
+    return true;
 }
 
