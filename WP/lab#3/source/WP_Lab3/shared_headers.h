0df6f3b2590e56d30d3dd3ca40becb67aae70ab3
Andrew <andrei.istratii@gmail.com>
2014-03-24 18:01:10 +0200

    add shared headers

---
+++ WP/lab#3/source/WP_Lab3/shared_headers.h
@@ -0,0 +1,32 @@
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <regex>
+
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+
+#include <io.h>
+#include <cstdio>
+#include <fcntl.h>
+
+
+#include "window.h"
+#include "frame_window.h"
+#include "workspace_window.h"
+#include "canvas.h"
+#include "backbuffer.h"
+#include "drawer.h"
+#include "drawable.h"
+#include "mouse.h"
+#include "tools.h"
+
+#include "resource.h"
+
