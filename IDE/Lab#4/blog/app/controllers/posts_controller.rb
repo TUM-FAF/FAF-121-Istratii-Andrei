1a9b758ca3afa3a82dd57ed98f19ecbc8ee470aa
Andrei Istratii <andrei.istratii@gmail.com>
2014-05-26 23:24:27 +0300

    Write lab 4

---
+++ IDE/Lab#4/blog/app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(secure_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(secure_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def secure_params
+    params.require(:post).permit(:title, :content)
+  end
+end
