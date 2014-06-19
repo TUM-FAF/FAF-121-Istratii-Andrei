6c298f7c974a1f30f6de90e9e421244847299b59
Andrew <andrei.istratii@gmail.com>
2014-06-19 08:51:18 +0300

    Upload ide lab 2

---
+++ IDE/Lab#2/source/IDE_Lab2/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IDE_Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ProgressBar currentPB;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            currentPB = PBAR1;
+            SLIDER.Value = 50;
+        }
+
+
+        private void SLIDER_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            currentPB.Value = SLIDER.Value;
+        }
+
+        private void BTN1_Click(object sender, RoutedEventArgs e)
+        {
+            currentPB = PBAR1;
+            SLIDER.Value = currentPB.Value;
+        }
+
+
+        private void BTN2_Click(object sender, RoutedEventArgs e)
+        {
+            currentPB = PBAR2;
+            SLIDER.Value = currentPB.Value;
+        }
+
+        private void BTN3_Click(object sender, RoutedEventArgs e)
+        {
+            currentPB = PBAR3;
+            SLIDER.Value = currentPB.Value;
+        }
+    }
+}
